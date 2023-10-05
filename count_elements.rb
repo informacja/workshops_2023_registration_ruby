1b11e47ad2b2c47244a84b2bd6a97b10425237b3
Piotr Wawryka <piotrwpwsz@gmail.com>
2023-10-05 08:01:35 +0200

    failure, w kolejce do lekarza :dvd:

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,9 @@
+class Object
+    def is_number?
+      to_f.to_s == to_s || to_i.to_s == to_s
+    end
+  end
+
 class CountElements
     
     attr_accessor :data
@@ -11,7 +17,12 @@ class CountElements
         s = @data.to_s.tr(',"[]','').strip.split
         u = s.uniq
         u.uniq { |x| 
-            hash.store(x, (s.count x))
+            if x.is_number?
+                y = x.to_i
+            else
+                y = x.to_s
+            end 
+            hash.store(y, (s.count x))
         }
         # puts hash
         hash
