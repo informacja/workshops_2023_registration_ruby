28656ab3458f730c96b958942071a910e1d69875
Piotr Wawryka <piotrwpwsz@gmail.com>
2023-10-05 06:16:28 +0200

    pisane w pociągu

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,32 @@
 class ValidatePassword
 
+    attr_accessor :data
+    # validate :check_valid_password
+
+    def initialize(parameter_list)
+        @data = parameter_list
+        @errors = false
+    end
+
+    def perform   
+
+        # hash = Hash.new
+        # s = @data.to_s.tr(',"[]','').strip.split
+        # u = s.uniq
+        # u.uniq { |x| 
+        #     hash.store(x, (s.count x))
+        # }
+       
+        
+        check_valid_password
+    end
+     
+    def check_valid_password
+        has_big_letter
+        return !@errors
+    end
+
+    def has_big_letter
+        /[[:upper:]]/.match(@data) unless @errors = true
+    end
 end
